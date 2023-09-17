983683ff0ee1ab7bfb7eb2c66336e54bc8df0a5e
Kaklesha <k-korni1ov@yandex.ru>
2023-09-17 21:52:24 +0400

    card

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import "package:http/http.dart";
-import 'dart:convert';
+import 'package:untitled/services/world_time.dart';
+
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -11,37 +11,16 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  String time = 'loading';
 
-  void getTime() async {
-
-    //simulate network request for a uaername
-    // String username =  await Future.delayed(Duration(seconds: 3),(){
-    //   return "yorys";
-
-    // Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
-    //   Map data =  jsonDecode(response.body);
-    //   print(data);
-    // print(data['title']);
-
-    // Response response = await get(Uri.parse('https://worldtimeapi.org/timezone/Europe/Saratov'));
-    //   Map data =  jsonDecode(response.body);
-    //
-
-    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/Europe/Saratov'));
-    Map data = jsonDecode(response.body);
-    //print(data);
-    //get properties from
-    String datetime = data["datetime"];
-    String offset = data['utc_offset'].substring(1,3);
-    print(datetime);
-    print(offset);
-
-    //create date time
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print (now);
-
-
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Saratov', flag: 'fff.jpg', url: 'Europe/Saratov');
+    await instance.getTime();
+     print(instance.time);
+    print('ddd');
+    setState(() {
+      time=instance.time;
+    });
   }
 
 
@@ -53,13 +32,17 @@ class _LoadingState extends State<Loading> {
     // TODO: implement initState
     super.initState();
     print("initState fun");
-    getTime();
+    setupWorldTime();
+
 
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('load')),
+      body: SafeArea(child: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Text(time),
+      )),
     );
   }
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,37 @@
+import "package:http/http.dart";
+import 'dart:convert';
+
+class WorldTime{
+
+  String location; //location name for th UI
+ late String time; //the time in that location
+  String flag; //url to an assets
+  String url; //location url for api
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future <void> getTime() async {
+
+
+    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
+    Map data = jsonDecode(response.body);
+    //print(data);
+    //get properties from
+    String datetime = data["datetime"];
+    String offset = data['utc_offset'].substring(1,3);
+    print(datetime);
+    print(offset);
+
+    //create date time
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print (now);
+
+    //set the time prop
+    time = now.toString();
+
+
+  }
+
+}
+
